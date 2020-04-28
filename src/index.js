33b80bba2d1003a30059a6d0b1777457cef48226
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-28 10:55:40 -0700

    mapping state to props, adding prop types, update code to use new props, web app functions correctly
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from redux;
+import { createStore } from 'redux';
 import reducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
 
