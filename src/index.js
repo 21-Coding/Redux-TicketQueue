1e7fe6ad14aa0aab4f536e713b5403998fda17bb
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-28 12:00:30 -0700

    add root reducer, test fails perhaps file naming convention wrong
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { createStore } from 'redux';
 import reducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
 
+
 const store = createStore(reducer);
 
 //notifies you when the reducer changes the state
