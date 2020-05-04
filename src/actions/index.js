0cc1be42759c7e8bb64449a5ee1a9359d43e8cc7
Jose Amesquita <joseamesquita11@gmail.com>
2020-05-04 11:32:57 -0700

    refactor add ticket to our application
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,18 +9,18 @@ export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
 
-export const addTicket = (ticket) => {
-  const { names, location, issue, formattedWaitTime, timeOpen, id } = ticket;
-  return {
-    type: 'ADD_TICKET',
-    names: names,
-    location: location,
-    issue: issue,
-    formattedWaitTime,
-    timeOpen: timeOpen,
-    id: id
-  }
-}
+// export const addTicket = (ticket) => {
+//   const { names, location, issue, formattedWaitTime, timeOpen, id } = ticket;
+//   return {
+//     type: 'ADD_TICKET',
+//     names: names,
+//     location: location,
+//     issue: issue,
+//     formattedWaitTime,
+//     timeOpen: timeOpen,
+//     id: id
+//   }
+// }
 
 export const updateTime = (id, formattedWaitTime) => ({
   type: 'UPDATE_TIME',
